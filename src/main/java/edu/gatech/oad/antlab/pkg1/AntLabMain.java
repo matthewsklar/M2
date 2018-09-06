9b25c12c61ff8262e3fda4d932d22d5e90ccb852
KrisLee4 <kristopherlee423@gmail.com>
2018-09-06 14:34:22 -0400

    Kris changing Matt Weissel's AntLabMain person.

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Matthew Weissel");
-                  toPrint += p5.toString("903282845");
+                  toPrint += p5.toString("mweissel3");
 
         System.out.println(toPrint);
 
